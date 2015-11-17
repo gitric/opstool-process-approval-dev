c029d4e9c002f716914e1c7e96b1ebe0f4004429
Ric <ric@appdevdesigns.net>
2015-11-17 15:33:50 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+opstool-process-approval ![Build Status](https://travis-ci.org/appdevdesigns/opstool-process-approval.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'opstool-process-approval/PluginController.inbox',
+  '/': {
+    controller: 'opstool-process-approval/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'opstool-process-approval/PluginController.signup',
+  'get /*(^.*)' : 'opstool-process-approval/PluginController.profile'
+
+  */
+
+
+};
+
